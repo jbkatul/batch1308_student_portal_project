875f57f9f957f2272744a166ec2bebe2a9e47c00
vaishnavisaindane9-sys <vaishnavisaindane9@gmail.com>
2026-03-02 01:48:22 +0530

    Portal: Add Student form

---
+++ portal/templates/portal/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from  .  import views
+
+urlpatterns = [
+    path("add/", views.add_student, name="add_student"),
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
