b3cd756bd014bca3aa925905786a8c6fa3ff8470
Rohan Kirdak <rohankirdak8756@gmail.com>
2026-03-01 14:07:37 +0530

    Add student form with list based logic

--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,27 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+# Temporary Database (List)
+student_db = []
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        course = request.POST.get("course")
+        email = request.POST.get("email")
+
+        student = {
+            "id": len(student_db) + 1,
+            "name": name,
+            "course": course,
+            "email": email
+        }
+
+        student_db.append(student)
+
+        return render(request, "portal/add_student.html", {
+            "success": True,
+            "students": student_db
+        })
+
+    return render(request, "portal/add_student.html")
