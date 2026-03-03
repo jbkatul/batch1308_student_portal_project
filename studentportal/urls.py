bec19816270fd607e10261f01697b19f7b137f75
harshadadeo2810 <harshadadeo84@gmail.com>
2026-03-03 23:40:47 +0530

    Fixed about URL issue and updated navigation

--- studentportal/urls.py
+++ studentportal/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('add-student/', views.add_student, name='add_student'),
     path('edit-student/<int:id>/', views.edit_student, name='edit_student'),
     path('delete-student/<int:id>/', views.delete_student, name='delete_student'),
+    path('about/', views.about, name='about'), 
 ]
