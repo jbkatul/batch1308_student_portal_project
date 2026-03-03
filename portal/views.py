f93b69ea77da79e4062d6decb39d4b81bb964148
DudhediyaYashasvi <dudhediyay@gmail.com>
2026-03-03 18:35:33 +0530

    Added Add Student page with validation

--- portal/views.py
+++ portal/views.py
@@ -1,22 +1,127 @@
 from django.shortcuts import render
+import re
 
-# Create your views here.
+students = [
+    {"id": 1, "name": "Rahul", "course": "Python", "email": "rahul@gmail.com", "class": "FY"},
+    {"id": 2, "name": "Sneha", "course": "Java", "email": "sneha@gmail.com", "class": "SY"},
+    {"id": 3, "name": "Amit", "course": "AI & ML", "email": "amit@gmail.com", "class": "TY"},
+]
+
+
 def home_view(request):
-    return render(request, 'portal/home.html')
+    courses = ["Python", "Java", "Software Testing", "AI & ML"]
+    return render(request, 'portal/home.html', {"courses": courses})
+
 
 def student_list_view(request):
-    return render(request, 'portal/student_list.html')
+    return render(request, 'portal/student_list.html', {"students": students})
+
+
+def add_student_view(request):
+    message = ""
+    error = ""
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        course = request.POST.get("course")
+        email = request.POST.get("email")
+        student_class = request.POST.get("class")
+
+        if not email:
+            error = "Email is required"
+
+        else:
+            student_data = {
+                "id": len(students) + 1,
+                "name": name,
+                "course": course,
+                "email": email,
+                "class": student_class
+            }
+
+            students.append(student_data)
+
+            print("NEW STUDENT ADDED:")
+            print(student_data)
+
+            message = "Student Added Successfully!"
+
+    return render(request, "portal/add_student.html",
+                  {"message": message, "error": error})
+
+
+def login_view(request):
+    message = ""
+    error = ""
+
+    if request.method == "POST":
+        email = request.POST.get("userid")
+        password = request.POST.get("password")
+
+        if len(password) < 8:
+            error = "Password must be at least 8 characters"
+        else:
+            login_data = {
+                "email": email,
+                "password": password
+            }
+
+            print("LOGIN DATA:")
+            print(login_data)
+
+            message = "Login Successful!"
+
+    return render(request, 'portal/login.html',
+                  {"message": message, "error": error})
+
+def register_view(request):
+    message = ""
+    error = ""
+
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        mobile = request.POST.get("mobile")
+        dob = request.POST.get("dob")
+        student_class = request.POST.get("class")
+        gender = request.POST.get("gender")
+
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        if not re.match(email_pattern, email):
+            error = "Invalid Email Format"
+
+        elif not mobile.isdigit() or len(mobile) != 10:
+            error = "Mobile number must be 10 digits"
+
+        elif len(password) < 8:
+            error = "Password must be at least 8 characters"
+
+        else:
+            register_data = {
+                "name": name,
+                "email": email,
+                "password": password,
+                "mobile": mobile,
+                "dob": dob,
+                "class": student_class,
+                "gender": gender
+            }
+
+            print("REGISTER DATA:")
+            print(register_data)
+
+            message = "Registration Successful!"
+
+    return render(request, 'portal/register.html',
+                  {"message": message, "error": error})
+
 
 def about_view(request):
     return render(request, 'portal/about.html')
-def home_view(request):
-    return render(request, 'portal/home.html')
-def student_list_view(request):
-
-    students = [
-        {"id": 1, "name": "Rahul", "course": "Python", "email": "rahul@gmail.com"},
-        {"id": 2, "name": "Sneha", "course": "Django", "email": "sneha@gmail.com"},
-        {"id": 3, "name": "Amit", "course": "Java", "email": "amit@gmail.com"},
-    ]
-
-    return render(request, 'portal/student_list.html', {"students": students})
+def student_detail_view(request, id):
+    student = next((s for s in students if s["id"] == id), None)
+    return render(request, 'portal/student_detail.html', {"student": student})
--- studentportal/urls.py
+++ studentportal/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for studentportal project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from portal.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('home/', home_view, name='home'),
-    path('student_list/', student_list_view, name='student_list'),
-    path('about/', about_view, name='about'),
     path('students/', student_list_view, name='student_list'),
+    path('student/<int:id>/', student_detail_view, name='student_detail'),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('about/', about_view, name='about'),
+    path('add-student/', add_student_view, name='add_student'),
 ]
